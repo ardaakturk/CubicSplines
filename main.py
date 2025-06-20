551cc8fbfcf3bbcb992ccbbd846b39483a36946c
Arda Aktürk <ardaaktrks@gmail.com>
2025-06-20 15:19:26 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+## Author: Arda Aktürk
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-def draw_segments(segments, title="Letter", lw=5):
+def draw_segments(segments, title="Shape", lw=5):
     """
-    segments: Her bir segment [x_start, x_end, fonksiyon] olarak tanımlanmalı.
-    Örnek: [1, 2, lambda x: 1 + 2*(x - 1)]
+    segments: Each sagment should be defined as [x_start, x_end, function]
+    Example: [1, 2, lambda x: 1 + 2*(x - 1)]
     """
     plt.figure(figsize=(6, 6))
 
@@ -22,14 +24,14 @@ def draw_segments(segments, title="Letter", lw=5):
 
 ############## LETTER A ###################
 segments_a = [
-    [1, 2, lambda x: 1 + 2 * (x-1)],         # sol bacak
-    [1.5, 2.5, lambda x: 2 + x*0],           # ara çizgi
-    [2, 3, lambda x: 1 - 2 * (x-3)]          # sağ bacak
+    [1, 2, lambda x: 1 + 2 * (x-1)],
+    [1.5, 2.5, lambda x: 2 + x*0],
+    [2, 3, lambda x: 1 - 2 * (x-3)]
 ]
-# draw_segments(segments_a, title="Letter A")
+draw_segments(segments_a, title="Letter A")
+
 
 ############## LETTER R ###################
-
 segments_r = [
     [2, 3, lambda x: 4.0 + 0.167*(x-2) + 0*(x-2)**2 + -0.417*(x-2)**3],
     [3, 3.5, lambda x: 3.75 + -1.083*(x-3) + -1.25*(x-3)**2 + 0.833*(x-3)**3],
@@ -38,10 +40,10 @@ segments_r = [
     [2, 3.5, lambda x: 2 - 2/1.5 * (x-2)],
     [2, 2.01, lambda x: 4 / 0.01 * (x-2)]
 ]
-# draw_segments(segments_r, title="Letter R")
+draw_segments(segments_r, title="Letter R")
+
 
 ############## LETTER D ###################
-
 segments_d = [
     [1, 2.414, lambda x: 5.0 + 0.292*(x-1) + 0*(x-1)**2 + -0.353*(x-1)**3],
     [2.414, 3, lambda x: 4.414 + -1.827*(x-2.414) + -1.499*(x-2.414)**2 + 0.853*(x-2.414)**3],
@@ -49,8 +51,8 @@ segments_d = [
     [2.414, 3, lambda x: 1.586 + 1.827*(x-2.414) + 1.499*(x-2.414)**2 + -0.853*(x-2.414)**3],
     [1, 1.01, lambda x: 1 + 4 / 0.01 * (x-1)]
 ]
+draw_segments(segments_d, title="Letter D")
 
-# draw_segments(segments_d, title="Letter D")
 
 ############## LETTER K ###################
 segments_k = [
@@ -58,8 +60,8 @@ segments_k = [
     [1, 2, lambda x: 2 + (x - 1)],
     [1, 2, lambda x: 2 - (x - 1)]
 ]
+draw_segments(segments_k, title="Letter K")
 
-# draw_segments(segments_k, title="Letter K")
 
 ############## LETTER U ###################
 segments_u = [
@@ -68,7 +70,7 @@ segments_u = [
     [2, 2.3, lambda x: 1.0 + 0*(x-2) + -1.287*(x-2)**2 + 7.994*(x-2)**3],
     [2.3, 3, lambda x: 1.1 + 1.386*(x-2.3) + 5.907*(x-2.3)**2 + -2.813*(x-2.3)**3]
 ]
-# draw_segments(segments_u, title="Letter U")
+draw_segments(segments_u, title="Letter U")
 
 
 ############## SMILE WITH BLUSH ###############
@@ -126,11 +128,10 @@ segments_swb = [
     *mouth
 ]
 
-# draw_segments(segments_swb, title="Smile with Blush")
+draw_segments(segments_swb, title="Smile with Blush")
 
 
 ############## SMILE WITH HALO ###############
-
 halo = [
     [2, 3, lambda x: 5.0 + 0.3*(x-2) + 0*(x-2)**2 + -0.1*(x-2)**3],
     [3, 4, lambda x: 5.2 + 0*(x-3) + -0.3*(x-3)**2 + 0.1*(x-3)**3],
@@ -149,7 +150,6 @@ draw_segments(segments_swh, title="Smile with Halo")
 
 
 ############## ANGRY FACE ###############
-
 sad_mouth = [
     [2.4, 3, lambda x: 1.7 + 2.0*(x-2.4) + 0*(x-2.4)**2 + -1.852*(x-2.4)**3],
     [3, 3.6, lambda x: 2.5 + 0*(x-3) + -3.333*(x-3)**2 + 1.852*(x-3)**3],
@@ -167,6 +167,11 @@ segments_angryface = [
 ]
 
 draw_segments(segments_angryface, title="Angry Face")
+
+
+############################# END ######################################
+
+
 
 
 
